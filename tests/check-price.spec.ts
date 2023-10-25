61f4f6d9604dfa919210a8da9b631fe87da25efd
Tanasie Vlad <tanasievladcristian@gmail.com>
2023-10-25 18:42:29 +0300

    Creted test for comparing the prices on emag, added util function read terms from the csv file

--- tests/check-price.spec.ts
+++ tests/check-price.spec.ts
@@ -1,12 +1,10 @@
 import {
   test,
   expect,
-  chromium,
-  firefox,
-  webkit,
-  BrowserType,
+  Page,
 } from '@playwright/test';
 import { getSearchTermsFromCSV } from '../util';
+import * as path from 'path';
 
 type Nullable<T> = {
     [P in keyof T]: T[P] | null;
@@ -21,36 +19,51 @@ interface BaseProductComparison {
 
 type ProductComparison = Nullable<BaseProductComparison>;
 
-const browsers: BrowserType[] = [chromium, firefox, webkit];
 const targetUrl = 'https://www.emag.ro/';
-const productComparisons: ProductComparison[] = [];
+const csvFilePath = path.join(__dirname, '..', 'searchTerms.csv');
+const productsToCompare: ProductComparison[] = [];
 
 test.describe('Verify the consistency of product details when searching for a product on EMAG', async () => {
-  for (const browserType of browsers) {
-    const browser = await browserType.launch();
-    const page = await browser.newPage();
-    const searchTerms = await getSearchTermsFromCSV('../search-terms.csv');
+  let page: Page;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    page.setDefaultTimeout(60000)
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test('should have the same product name on the search page and on the product page', async () => {
+    const searchTerms = await getSearchTermsFromCSV(csvFilePath);
 
     for (const term of searchTerms) {
       await page.goto(targetUrl);
       await page.fill('input[type="search"][id="searchboxTrigger"][name="query"]', term);
       await page.click('button[class="btn btn-default searchbox-submit-button"]');
       await page.waitForSelector('.card-v2-title');
+
       const productName = await page.$eval('.card-v2-title', el => el.textContent);
       const productPrice = await page.$eval('.product-new-price', el => el.textContent);
-      await page.click('.card-v2-title');
-      const productPageTitle = await page.$eval('.page-title', el => el.textContent);
-      const productPagePrice = await page.$eval('.product-new-price', el => el.textContent);
 
-      productComparisons.push({
+      await page.click('a[class="card-v2-title semibold mrg-btm-xxs js-product-url"]');
+      await page.waitForSelector('.page-title');
+
+      const productPageTitle = await page.$eval('h1[class="page-title"]', el => el.textContent);
+      const productPagePrice = await page.$eval('p[class="product-new-price"]', el => el.textContent);
+
+      const productComparison: ProductComparison = {
         productName,
         productPrice,
-        productPageTitle,
+        productPageTitle: productPageTitle!.replace(/\n/g, '').trim(),
         productPagePrice,
-      });
-      console.log(productComparisons);
+      };
+      productsToCompare.push(productComparison);
     }
 
-    test(`should have the same price on product page and listing page on browser ${browserType.name()}`, () => {})
-  }
+    for (const product of productsToCompare) {
+      expect(product.productName).toBe(product.productPageTitle);
+    }
+  });
 });
--- util/get-search-terms.ts
+++ util/get-search-terms.ts
@@ -4,5 +4,5 @@ import * as Papa from 'papaparse';
 export async function getSearchTermsFromCSV(filePath: string): Promise<string[]> {
   const csvData = fs.readFileSync(filePath, 'utf8');
   const results = Papa.parse(csvData, { header: true });
-  return results.data.map(row => row['searchTerm']); // Assuming the CSV column name is "searchTerm"
+  return results.data.map((row: any) => row['searchTerm']); // Assuming the CSV column name is "searchTerm"
 }
